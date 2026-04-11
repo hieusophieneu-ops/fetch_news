5d494a4ffb477b1d8ef978b9110ace3030b2210d
hieusophieneu-ops <hieusophieneu@gmail.com>
2026-04-11 16:18:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 import requests
 import xml.etree.ElementTree as ET
+import os
 
 app = Flask(__name__)
 
@@ -46,3 +47,7 @@ def get_news():
 
     except Exception as e:
         return jsonify({"error": str(e)})
+        
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
